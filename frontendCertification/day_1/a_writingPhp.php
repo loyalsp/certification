e6803725a7bf4f3a9fcd35e8aa55642df38e9601
Adnan Rasheed <adnann.rasheed@gmail.com>
2016-03-20 20:24:38 +0500

    test

--- frontendCertification/day_1/a_writingPhp.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*php script is written in this tag<?php    ?> */
-//and this is a single comment line.
-/*this is multiple line
- comment
-*/
-echo "hello world";
-?>
--- frontendCertification/day_1/e_practice.php
+++
@@ -1,7 +0,0 @@
-<?php
-$str = "hello World";
-$res = stripos($str,"r");
-echo $res;
-$res1 = str_ireplace("hello", "My", $str);
-echo $res1;
-?>
